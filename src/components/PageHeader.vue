6e764fa6ec7c6947c0c38627f2a9c8de639bb502
Alexander Schwartzberg <aeksco@gmail.com>
2017-11-20 01:11:54 -0500

    Added PageHeader component

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row">
+    <div class="col-lg-12">
+      <h3>{{ title }}</h3>
+      <hr>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageHeader',
+  props: [ 'title' ]
+}
+</script>
